302a6516b7a3bd0ed0d84b4e8d8865b1c2896627
nawumi <nawumicodex@gmail.com>
2019-06-13 22:58:59 +0700

    update card/body

--- application/views/front/cart/body.php
+++ application/views/front/cart/body.php
@@ -1,116 +1,129 @@
 <?php $this->load->view('front/header'); ?>
 <?php $this->load->view('front/navbar'); ?>
-<section class="faq_area section--padding">
+<section class="breadcrumb-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="breadcrumb">
+                    <ul>
+                        <li>
+                            <a href="index.html">Home</a>
+                        </li>
+                        <li class="active">
+                            <a href="#">Checkout</a>
+                        </li>
+                    </ul>
+                </div>
+                <h1 class="page-title">Checkout</h1>
+            </div>
+            <!-- end /.col-md-12 -->
+        </div>
+        <!-- end /.row -->
+    </div>
+    <!-- end /.container -->
+</section>
+<section class="products section--padding2">
     <div class="container">
         <div class="row">
             <div class="col-lg-8">
-                <div class="cardify faq_module ">
-                    <div class="faq-title">
-                        <span class="lnr lnr-cart"></span>
-                        <h4>Total Keranjang (<?php echo $total_cart_navbar ?>)</h4>
+                <?php foreach ($cart_data as $cart){ ?>
+                <div class="product product--list product--list-small">
+                    <div class="product__thumbnail">
+
+                        <?php
+                            if(empty($cart->foto)) {echo "<img  src='".base_url()."assets/template/fronted1/images/produk_kosong.png'>";}
+                            else { echo "<img  src='".base_url()."assets/images/produk/".$cart->foto.'_thumb'.$cart->foto_type."'> ";}
+                            ?>
+                        <form action="<?php echo base_url('cart/update/').$cart->produk_id ?>" method="post">
+                            <div class="prod_btn">
+                                <a href="#" class="btn btn-sm btn--round btn-danger" data-target="#myModal2"
+                                    data-toggle="modal">Hapus</a>
+
+                            </div>
                     </div>
-                    <div class="card table-responsive padding">
-                        <div class="card-body m-1">
-                            <table class="table table-striped border-left">
-                                <thead>
-                                    <tr>
-                                        <th class="">No.</th>
-                                        <th class="">Barang</th>
-                                        <th class="">Harga/Total</th>
-                                        <th class="text-center" width="20%">Qty</th>
-                                        <th class="text-center" width="20%">Option</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php $no=1; foreach ($cart_data as $cart){ ?>
-                                    <tr>
-                                        <td class=""><?php echo $no++ ?></td>
-                                        <td><?php echo character_limiter($cart->judul_produk,40) ?></td>
-                                        <td class="">
-                                            <?php echo number_format($cart->harga_diskon) ?>/
-                                            <b><?php echo number_format($cart->subtotal) ?></b>
-                                        </td>
-                                        <form action="<?php echo base_url('cart/update/').$cart->produk_id ?>"
-                                            method="post">
-                                            <td class="">
+                    <div class="modal fade rating_modal item_remove_modal" id="myModal2" tabindex="-1" role="dialog"
+                        aria-labelledby="myModal2">
+                        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h3 class="modal-title">Apakah Kamu yakin Menghapus</h3>
+                                    <p> Projek <b><?php echo character_limiter($cart->judul_produk,40) ?></b> Ini Dari
+                                        Keranjang Anda ?</p>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <!-- end /.modal-header -->
+
+                                <div class="modal-body">
+                                    <button name="delete" type="submit"
+                                        class="btn btn--icon btn-sm btn--round btn-danger">
+                                        <span class="lnr lnr-trash"></span>Saya Yakin</button>
+                                    <button class="btn btn--round modal_close" data-dismiss="modal">Batal</button>
+                                </div>
+                                <!-- end /.modal-body -->
+                            </div>
+                        </div>
+                    </div>
+                    <div class="product__details text-center">
+                        <div class="product-desc">
+                            <a href="#" class="product_title">
+                                <h4><?php echo character_limiter($cart->judul_produk,40) ?></h4>
+                            </a>
+                            <p><?php echo character_limiter($cart->deskripsi,20) ?></p>
+                            <ul class="titlebtm">
+                                <li class="product_cat">
+                                    <a href="#">
+                                        <span class="lnr lnr-cart"></span> QTY : <?php echo $cart->total_qty ?>
+                                    </a>
+                                    <a href="#" id="drop2" class="dropdown-trigger" data-toggle="dropdown"
+                                        aria-haspopup="true" aria-expanded="true">
+                                        <span class="lnr lnr-pencil setting-icon"></span>
+                                    </a>
+                                    <div class="options dropdown-menu" aria-labelledby="drop2">
+                                        <ul>
+                                            <li>
                                                 <input type="hidden" name="produk_id"
                                                     value="<?php echo $cart->produk_id ?>">
+                                            </li>
+                                            <li class="m-3">
                                                 <input type="number" name="qty" value="<?php echo $cart->total_qty ?>">
-                                            </td>
-                                            <td class="">
-                                                <div class="btn-group" role="group">
-                                                    <button id="btnGroupDrop1"
-                                                        class="btn btn-sm btn-success dropdown-toggle"
-                                                        data-toggle="dropdown" aria-haspopup="true"
-                                                        aria-expanded="false"> Option </button>
-                                                    <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                                                        <button type="submit" name="update" class="dropdown-item"><i
-                                                                class="fa fa-refresh"></i> Simpan Perubahan</button>
-                                                        <button type="submit" name="delete" class=" dropdown-item"><i
-                                                                class="fa fa-remove"></i> Hapus </button>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                        </form>
-                                        <?php } ?>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                    <div class="faqs">
-                        <div class="panel-group accordion" role="tablist" id="accordion">
-                            <div class="panel accordion__single" id="panel">
-                                <div class="single_acco_title">
-                                    <h4>
-                                        <a data-toggle="collapse" href="#collapse3" class="collapsed"
-                                            aria-expanded="false" data-target="#collapse3" aria-controls="collapse3">
-                                            <span>Data Penerima</span>
-                                            <i class="lnr lnr-chevron-down indicator"></i>
-                                        </a>
-                                    </h4>
-                                </div>
-                                <?php echo form_open('cart/checkout') ?>
-                                <?php if(!empty($customer_data->id_trans)){ ?>
-                                <input type="hidden" name="id_trans" value="<?php echo $customer_data->id_trans ?>">
-                                <input type="hidden" name="total" id="total"
-                                    value="<?php echo $total_berat_dan_subtotal->subtotal ?>" />
-                                <input type="hidden" name="ongkir" id="ongkir" value="0" />
-                                <?php } ?>
-                                <div id="collapse3" class="panel-collapse collapse" aria-labelledby="panel"
-                                    data-parent="#accordion">
-                                    <div class="panel-body">
-                                        <div class="col-lg-12 col-md-12">
-                                            <div class="contact_tile">
-                                                <span class="tiles__icon lnr lnr-user"></span>
-                                                <h4 class="tiles__title">
-                                                    <?php if($customer_data){echo $customer_data->name;} ?></h4>
-                                                <div class="tiles__content">
-                                                    <p> <b>Telepon :</b>
-                                                        <?php if($customer_data){echo $customer_data->phone;} ?></p>
-                                                    <address> <b>Alamat : </b>
-                                                        <?php if($customer_data){echo $customer_data->address.', '.$customer_data->nama_kota.', '.$customer_data->nama_provinsi;}?>
-                                                    </address>
-                                                </div>
-                                            </div>
-                                            <!-- end /.contact_tile -->
-                                        </div>
+                                            </li>
+                                            <li class="text-center">
+                                                <button name="update" type="submit"
+                                                    class="btn btn--icon btn-sm btn--round btn-secondary">
+                                                    <span class="lnr lnr-pencil"></span>Update</button>
+                                            </li>
+                                        </ul>
                                     </div>
-                                </div>
-                            </div>
-                            <!-- end /.accordion__single -->
+                                </li>
+                            </ul>
                         </div>
-                        <!-- end /.accordion -->
+                        <div class="product-purchase ml-4">
+                            <div class="price_love">
+                                <span>Total : <?php echo number_format($cart->subtotal) ?>
+                                </span>
+                                <b><?php echo number_format($cart->harga_diskon) ?>/PCS</b>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <!-- end /.cardify -->
+                </form>
+                <?php } ?>
             </div>
-            <!-- end /.col-md-8 -->
             <div class="col-lg-4">
                 <aside class="sidebar sidebar--single-product">
                     <div class="information_module payment_options">
                         <div class="toggle_title">
                             <h4>Pilih Kurir Pengiriman</h4>
                         </div>
+                        <?php echo form_open('cart/checkout') ?>
+                        <?php if(!empty($customer_data->id_trans)){ ?>
+                        <input type="hidden" name="id_trans" value="<?php echo $customer_data->id_trans ?>">
+                        <input type="hidden" name="total" id="total"
+                            value="<?php echo $total_berat_dan_subtotal->subtotal ?>" />
+                        <input type="hidden" name="ongkir" id="ongkir" value="0" />
+                        <?php } ?>
                         <div class="select-wrap select-wrap2">
                             <select name="kurir" class="kurir">
                                 <option value="">Pilih Jenis Kurir</option>
@@ -144,13 +157,14 @@
                             <li>
                                 <p>
                                     <span class="lnr lnr-bookmark"></span>Subtotal</p>
-                                <span><?php echo $total_berat_dan_subtotal->subtotal ?></span>
+                                <span><sup>Rp</sup>
+                                    <?php echo number_format($total_berat_dan_subtotal->subtotal) ?></span>
                             </li>
                             <li>
                                 <p>
                                     <span class="lnr lnr-tag"></span>Grand Total</p>
                                 <span>
-                                    <h3><sup>Rp</sup><b id="grandtotal"></b></h3>
+                                    <h3><sup>Rp</sup> <b id="grandtotal"></b></h3>
                                 </span>
                             </li>
                         </ul>
